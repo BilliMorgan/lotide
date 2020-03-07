c721fe4130f469783375b03ed653fac8aa162505
Andrii Shymko <shimko20@gamil.com>
2020-03-07 01:29:53 +0000

    final stage of test programm

--- eqArrays.js
+++ eqArrays.js
@@ -19,13 +19,14 @@ const eqArrays = function (actual, expected) {
     output = false;
   }
   return output;
-}
+};
 
 
 console.log(eqArrays([1, 2, 3], [1, 2, 3])) // => true
 console.log(eqArrays([1, 2, 3], [3, 2, 1])) // => false
 
-eqArrays(["1", "2", "3"], ["1", "2", "3"]) // => true
-eqArrays(["1", "2", "3"], ["1", "2", 3]) // => false
+console.log(eqArrays(["1", "2", "3"], ["1", "2", "3"])) // => true
+console.log(eqArrays(["1", "2", "3"], ["1", "2", 3])) // => false
 
 assertEqual(eqArrays([1, 2, 3], [1, 2, 3,]), true);
+assertEqual(eqArrays([1, 2, 3], [3, 2, 1]), false);
