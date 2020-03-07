4bff613261768f8d9e58203a7111e88b4ae529f0
Andrii Shymko <shimko20@gamil.com>
2020-03-07 04:43:39 +0000

    finalazed cod with test implemented

--- map.js
+++ map.js
@@ -1,11 +1,9 @@
 const words = ["ground", "control", "to", "major", "tom"];
-
-const map = function(array, callback) {
+const map = function (array, callback) {
   // console.log('array:', array);
   // console.log('callback:', callback);
-  
   const results = [];
-  for (let item of array){
+  for (let item of array) {
     results.push(callback(item));
     //console.log('item BEFORE: ', item);
     //console.log('ietm AFTER', callback(item));
@@ -14,4 +12,28 @@ const map = function(array, callback) {
   return results;
 }
 const results1 = map(words, word => word[0]);
-//console.log(results1);
+console.log(results1);
+
+// test function bellow
+const eqArrays = function (actual, expected) {
+  let output = true;
+  if (actual.length === expected.length) {
+    for (let i = 0; i < actual.length; i++) {
+      if (actual[i] !== expected[i]) {
+        output = false;
+      }
+    }
+  }
+  else {
+    output = false;
+  }
+  return output;
+};
+const assertArraysEqual = function (array1, array2) {
+  if (eqArrays(array1, array2) === true) {
+    return console.log(`✅✅✅Assertion Passed: ${array1} === ${array2}`);
+  } else {
+    return console.log(`🛑🛑🛑Assertion Failed: ${array1} !== ${array2}`);
+  }
+};
+assertArraysEqual(results1, [ 'g', 'c', 't', 'm', 'r' ]);
