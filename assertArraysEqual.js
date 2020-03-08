d4d9b329d6e6c7a60acb7ada3e49ac39b0951af2
BilliMorgan <shimko20@gmail.com>
2020-03-08 22:45:36 +0000

    final stage with modules implemented

--- assertArraysEqual.js
+++ assertArraysEqual.js
@@ -20,9 +20,11 @@ const assertArraysEqual = function (array1, array2) {
   }
 };
 
-assertArraysEqual([1, 2, 3], [1, 2, 3, 8, 0]); // fail
-assertArraysEqual([1, 2, 3], [1, 2, 3]);       // pass
-assertArraysEqual(["h", "e", "l", "l"], ["h", "e", "l", "l"]); //pass
-assertArraysEqual(["h", "e", "l", "l"], ["h", "e", "l", "o"]);  //fail
-//assertArraysEqual([], 2, 3], [[], 2, 3]);  
-//assertArraysEqual([1, 2, 3], [1, 2, 3]);
+// assertArraysEqual([1, 2, 3], [1, 2, 3, 8, 0]); // fail
+// assertArraysEqual([1, 2, 3], [1, 2, 3]);       // pass
+// assertArraysEqual(["h", "e", "l", "l"], ["h", "e", "l", "l"]); //pass
+// assertArraysEqual(["h", "e", "l", "l"], ["h", "e", "l", "o"]);  //fail
+// assertArraysEqual([[], 2, 3], [[], 2, 3]);  
+// assertArraysEqual([1, 2, 3], [1, 2, 3]);
+
+module.exports = assertArraysEqual
---
+++ middle.js
@@ -0,0 +1,15 @@
+const assertArraysEqual = require('./assertArraysEqual.js');
+
+const middle = function(array) {
+  let middle = [];
+  if (array.length % 2 === 0) {
+    let number = array.length / 2;
+    middle.push(array[Math.floor(number - 1)],array[Math.floor(number)]);
+  } else {
+    let number = array.length / 2;
+
+    middle.push(array[Math.floor(number)]);
+  }
+  return middle;
+};
+assertArraysEqual(middle([1, 2, 3, 4, 5, 6, 7, 8,]), [4, 5]);       // pass
