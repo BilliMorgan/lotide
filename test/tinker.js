f889f9f33f6981af1e41d72051bcf04993100af4
BilliMorgan <shimko20@gmail.com>
2020-03-13 13:28:57 +0000

    added findKey

--- test/tinker.js
+++ test/tinker.js
@@ -1,46 +1,73 @@
-// let arrTinker = [1, 2, 3, 4, 5, 6]
+// // let arrTinker = [1, 2, 3, 4, 5, 6]
 
-// return console.log(Array.isArray([1, 2, 3, 4, 5, 6]))
-
-
-// const object1 = {
-//   a: 'somestring',
-//   b: 42,
-//   c: false
-// };
-
-// console.log(Object.keys(object1));
-// // expected output: Array ["a", "b", "c"]
-
-// let cat = 'Miaow';
-// let dog = 'Woof';
-// let bird = 'Peet peet';
-
-// let someObject = {
-//   cat,
-//   dog,
-//   bird
-// }
-
-// console.log(someObject);
-
-// const obj = { x: 4, y: 1 };
-// const {x,y} = obj;
-// console.log(x); // 4
-// console.log(y); // 1
+// // return console.log(Array.isArray([1, 2, 3, 4, 5, 6]))
 
 
-const index = require('../index')
-const assert = require('chai').assert;
+// // const object1 = {
+// //   a: 'somestring',
+// //   b: 42,
+// //   c: false
+// // };
 
-describe('#middletest', () => {
-  it('should return [4, 5] for [1, 2, 3, 4, 5, 6, 7, 8,]', () => {
-    assert.deepEqual(index.middle([1, 2, 3, 4, 5, 6, 7, 8,]), [4, 5])
-  });
+// // console.log(Object.keys(object1));
+// // // expected output: Array ["a", "b", "c"]
 
-  it('should return [4] for [1, 2, 3, 4, 5, 6, 7]', () => {
-    assert.deepEqual(index.middle([1, 2, 3, 4, 5, 6, 7]), [4])
-  });
+// // let cat = 'Miaow';
+// // let dog = 'Woof';
+// // let bird = 'Peet peet';
+
+// // let someObject = {
+// //   cat,
+// //   dog,
+// //   bird
+// // }
+
+// // console.log(someObject);
+
+// // const obj = { x: 4, y: 1 };
+// // const {x,y} = obj;
+// // console.log(x); // 4
+// // console.log(y); // 1
+
+
+// const index = require('../index')
+// const assert = require('chai').assert;
+
+// describe('#middletest', () => {
+//   it('should return [4, 5] for [1, 2, 3, 4, 5, 6, 7, 8,]', () => {
+//     assert.deepEqual(index.middle([1, 2, 3, 4, 5, 6, 7, 8,]), [4, 5])
+//   });
+
+//   it('should return [4] for [1, 2, 3, 4, 5, 6, 7]', () => {
+//     assert.deepEqual(index.middle([1, 2, 3, 4, 5, 6, 7]), [4])
+//   });
   
-});
+// });
 
+let Object = {key: {nestedKey: 1}};
+console.log(Object.key["nestedKey"])
+
+const objKeys = Object.keys(ObjectItself);
+console.log(objKeys)
+const blo = false;
+const value = Object.values(ObjectItself)
+console.log(value)
+
+for (y of value){
+  for (z in y){
+    if (y[z] === 2){
+      console.log(y);
+    }
+  }
+console.log(y[z])
+  }
+
+for (key of value){
+  if (ObjectItself[key.""] === condition){
+    console.log(ObjectItself[key])
+    console.log(condition)
+  return blo = true;
+}
+}
+
+//const starsValue = Object.values(.Object)
